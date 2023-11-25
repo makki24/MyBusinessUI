319b26758be73bfc0237014dca962bb1c5bb384c
makki24 <maqhti@gmail.com>
2023-11-25 18:15:27 +0530

    added loading and pull to refresh: MYB-8-v2

--- screens/AddRoleScreen.tsx
+++ screens/AddRoleScreen.tsx
@@ -1,6 +1,6 @@
 // src/screens/AddRoleScreen.tsx
 import React, { useState } from 'react';
-import { View, StyleSheet } from 'react-native';
+import { View, StyleSheet, Text, ActivityIndicator } from 'react-native';
 import { useRecoilState } from 'recoil';
 import Input from '../components/common/Input';
 import Button from '../components/common/Button';
@@ -15,9 +15,13 @@ interface AddRoleScreenProps {
 const AddRoleScreen: React.FC<AddRoleScreenProps> = ({ navigation }) => {
     const [roleName, setRoleName] = useState('');
     const [roles, setRoles] = useRecoilState(rolesState);
+    const [error, setError] = useState<string | null>(null);
+    const [isLoading, setIsLoading] = useState(false);
 
     const handleAddRole = async () => {
         try {
+            setIsLoading(true); // Set loading to true
+
             // Call your API service to add a new role
             const newRole = await RolesService.addRole({ roleName });
 
@@ -28,11 +32,28 @@ const AddRoleScreen: React.FC<AddRoleScreenProps> = ({ navigation }) => {
             navigation.goBack();
         } catch (error) {
             console.error('Error adding role:', error);
+            setError(error.response.data); // Set the error message
+        } finally {
+            setIsLoading(false); // Set loading to false regardless of success or failure
         }
     };
 
     return (
         <View style={styles.container}>
+            {isLoading && (
+                <View style={styles.loadingContainer}>
+                    <Text>
+                        <ActivityIndicator size="large" color="#0000ff" /> {/* Show spinner if loading */}
+                    </Text>
+                </View>
+            )}
+
+            {error && (
+                <View style={styles.errorContainer}>
+                    <Text style={styles.errorText}>{error}</Text>
+                </View>
+            )}
+
             <Input
                 placeholder="Enter role name"
                 value={roleName}
@@ -48,6 +69,21 @@ const styles = StyleSheet.create({
         flex: 1,
         padding: 16,
         justifyContent: 'center',
+    },
+    loadingContainer: {
+        ...StyleSheet.absoluteFillObject,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: 'rgba(0, 0, 0, 0.5)',
+    },
+    errorContainer: {
+        backgroundColor: 'red',
+        padding: 10,
+        marginBottom: 10,
+        borderRadius: 5,
+    },
+    errorText: {
+        color: 'white',
     },
 });
 
