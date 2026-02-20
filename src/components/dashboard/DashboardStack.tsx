43605b187cf79da0e7dd5771ecc1bb2824387ebe
makki24 <maqhti@gmail.com>
2026-02-20 18:09:49 +0530

    fixed issues

--- src/components/dashboard/DashboardStack.tsx
+++ src/components/dashboard/DashboardStack.tsx
@@ -3,12 +3,16 @@ import { createStackNavigator } from "@react-navigation/stack";
 import ModernDashboardScreen from "./ModernDashboardScreen";
 import LineChartScreen from "./LineChartScreen";
 import SummaryByType from "./SummaryByType";
+import CustomHeader from "../common/CustomHeader";
 
 const Stack = createStackNavigator();
 
 const DashboardStack = () => {
   return (
-    <Stack.Navigator id="DashboardStack" screenOptions={{ headerShown: false }}>
+    <Stack.Navigator
+      id="DashboardStack"
+      screenOptions={{ header: () => <CustomHeader /> }}
+    >
       <Stack.Screen name="Dashboard" component={ModernDashboardScreen} />
       <Stack.Screen name="LineChart" component={LineChartScreen} />
       <Stack.Screen name="SummaryByType" component={SummaryByType} />
