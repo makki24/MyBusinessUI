92aa883bdccb8c3373702a87bb8860c483404c93
makki24 <maqhti@gmail.com>
2026-05-14 16:21:42 +0530

    fixed theme

--- App.tsx
+++ App.tsx
@@ -2,6 +2,7 @@
 import "react-native-gesture-handler"; // Must be at the very top
 import "react-native-reanimated";
 import React from "react";
+import { useColorScheme } from "react-native";
 import { Provider as PaperProvider } from "react-native-paper";
 import { RecoilRoot } from "recoil";
 
@@ -11,13 +12,16 @@ import AppContent from "./src/components/AppContent";
 import { enGB, registerTranslation } from "react-native-paper-dates";
 registerTranslation("en-GB", enGB);
 
-import { theme } from "./src/styles/theme";
+import { theme, darkTheme } from "./src/styles/theme";
 
 const App = () => {
+  const colorScheme = useColorScheme();
+  const currentTheme = colorScheme === "dark" ? darkTheme : theme;
+
   return (
     <GestureHandlerRootView style={{ height: "100%", width: "100%" }}>
       <RecoilRoot>
-        <PaperProvider theme={theme}>
+        <PaperProvider theme={currentTheme}>
           <BottomSheetModalProvider>
             <AppContent />
           </BottomSheetModalProvider>
